2bdfbd3e7c4e5d8ae4cd972584fa792cd7c4383c
José Bieco <jo_bieco@hotmail.com>
2024-09-14 09:14:54 -0300

    feat(src/components/CustomAlert): criacao de um componente para lidar com os alertas/mensagens que teremos no sistema

---
+++ src/components/CustomAlert.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Alert from "@mui/material/Alert";
+import IconButton from "@mui/material/IconButton";
+import Collapse from "@mui/material/Collapse";
+import CloseIcon from "@mui/icons-material/Close";
+
+export interface IAlertProps {
+  type: "success" | "info" | "warning" | "error";
+  message: string;
+  setFatherState?: () => void;
+}
+
+export default function CustomAlert(props: IAlertProps) {
+  const [open, setOpen] = React.useState(true);
+
+  const handleExited = () => {
+    // Executa quando a animação de colapso termina
+    if (props.setFatherState) {
+      props.setFatherState(); // Executa a função passada após o fechamento
+    }
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Collapse in={open} onExited={handleExited}>
+        <Alert
+          severity={props.type}
+          variant="filled"
+          action={
+            <IconButton
+              aria-label="close"
+              color="inherit"
+              size="small"
+              onClick={() => setOpen(false)}
+            >
+              <CloseIcon fontSize="inherit" />
+            </IconButton>
+          }
+          sx={{ mb: 2 }}
+        >
+          {props.message}
+        </Alert>
+      </Collapse>
+    </Box>
+  );
+}
