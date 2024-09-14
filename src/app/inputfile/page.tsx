6977d19c147e4a9b7923940fc73892fbb50db02a
José Bieco <jo_bieco@hotmail.com>
2024-09-14 09:15:35 -0300

    refactor(src/app/inputfile): refatoracao da pagina de carregamento de dados, aplicando um novo layout e adicionando o nome do arquivo a ser carregado.

--- src/app/inputfile/page.tsx
+++ src/app/inputfile/page.tsx
@@ -1,94 +1,50 @@
 "use client";
 
-import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Button from "@mui/material/Button";
+import {
+  Button,
+  Box,
+  Typography,
+  CircularProgress,
+} from "@mui/material";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
-import PublishRoundedIcon from "@mui/icons-material/PublishRounded";
-import CloseIcon from "@mui/icons-material/Close";
-import { Alert, Box, Collapse, IconButton, Stack, Typography } from "@mui/material";
-import { useGlobalContext } from "@/context/Global";
-import { motion } from "framer-motion";
-
+import { useState } from "react";
 import {
   processAndUpdateState,
   processAtribuicoes,
   processDisciplinas,
   processDocentes,
   processFormularios,
-} from "./UpdateState";
+} from "../inputfile/UpdateState";
+import { useGlobalContext } from "@/context/Global";
+import CustomAlert, { IAlertProps } from "@/components/CustomAlert";
 
-const VisuallyHiddenInput = styled("input")({
-  clip: "rect(0 0 0 0)",
-  clipPath: "inset(50%)",
-  height: 1,
-  overflow: "hidden",
-  position: "absolute",
-  bottom: 0,
-  left: 0,
-  whiteSpace: "nowrap",
-  width: 1,
-});
+export default function Teste() {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [uploading, setUploading] = useState(false);
+  const [alert, setAlert] = useState<IAlertProps|null>(null);
+  const { setAtribuicoes, setDisciplinas, setDocentes, setFormularios } =
+    useGlobalContext();
 
-export default function InputFileUpload() {
-  const [activeLoader, setActiveLoader] = React.useState<boolean>(true); // Controla se o botão de carregar estará habilitado
-  const [selectedFile, setSelectedFile] = React.useState<File | null>(null); // Armazena o arquivo selecionado
-  //const [fileContent, setFileContent] = React.useState(null); // Armazena o conteúdo do arquivo JSON
-  const [open, setOpen] = React.useState(false); // Alerta de erro
-  const [message, setMessage] = React.useState("");
-  const [loaded, setLoaded] = React.useState(false)
-
-  const {
-    // atribuicoes,
-    setAtribuicoes,
-    //disciplinas,
-    setDisciplinas,
-    // docentes,
-    setDocentes,
-    // formularios,
-    setFormularios,
-  } = useGlobalContext();
-
-  // Função para lidar com a seleção de arquivo
-  const setFileState = (files: FileList | null) => {
+  const handleFileSelect = (files: FileList | null) => {
+    // Verifica se realmente existe um arquivo
     if (files && files.length > 0) {
-      // Altera o state da variávle que controla o texto de "carregado com sucesso" para falso
-      setLoaded(false)
-
       const file = files[0];
       // Verifica se o arquivo é do tipo JSON
       if (file.type === "application/json") {
         setSelectedFile(file);
-        setActiveLoader(false); // Habilita o botão "Carregar arquivo"
       } else {
-        //alert("Por favor, selecione um arquivo JSON.");
-        setMessage("Por favor, selecione um arquivo JSON.");
-        setOpen(true);
-        setActiveLoader(true); // Desabilita o botão caso não seja JSON
+        // Apresentar mensagem de erro na tela
+        //console.log("Por favor, selecione um arquivo JSON.");
+        setAlert({type: "warning", message: "Por favor, selecione um arquivo JSON."})
       }
     }
   };
 
-  // Função para lidar com o clique no botão de carregar
-  const handleFileUpload = () => {
-    if (selectedFile) {
-      const reader = new FileReader();
-      reader.onload = (event) => {
-        try {
-          const json = JSON.parse(event.target?.result as string);
-          //setFileContent(json); // Armazena o conteúdo JSON
-          console.log("Conteúdo do arquivo JSON:", json);
-          loadContext(json);
-        } catch (error) {
-          setMessage("Erro ao processar o arquivo JSON.");
-          setOpen(true);
-        }
-      };
-      reader.readAsText(selectedFile); // Lê o arquivo como texto
-      setLoaded(true)
-    }
-  };
-
+  /**
+   *  Carrega o arquivo JSON no state global
+   * @param json Informações contidas no arquivo anteriormente lido
+   */
   const loadContext = (json) => {
     // Processa e atualiza o estado de "docentes"
     processAndUpdateState(
@@ -123,81 +79,89 @@ export default function InputFileUpload() {
     );
   };
 
+  // Importa os dados do arquivo para o state global
+  const handleFileUpload = () => {
+    if (selectedFile) {
+      setUploading(true);
+      const reader = new FileReader();
+      reader.onload = (event) => {
+        try {
+          const json = JSON.parse(event.target?.result as string);
+          loadContext(json);
+          setSelectedFile(null);
+        } catch (error) {
+          setAlert({type: "error", message: "Erro ao processar o arquivo JSON."})
+        }
+      };
+      reader.readAsText(selectedFile);
+    }
+    setUploading(false);
+    setAlert({type:"success", message: "Arquivo carregado com sucesso."})
+  };
+
   return (
     <Box
-      sx={{ width: "100%" }}
       display="flex"
       flexDirection="column"
       alignItems="center"
+      justifyContent="center"
+      p={4}
+      border="1px solid #ccc"
+      borderRadius={2}
+      maxWidth={400}
+      mx="auto"
+      boxShadow={2}
     >
-      <Box sx={{ width: "100%" }} display="flex" justifyContent="center">
-        <Stack
-          sx={{ width: "50%" }}
-          display="flex"
-          justifyContent="space-between"
-          flexDirection="row"
-          flexWrap="wrap"
+      <Typography variant="h6" gutterBottom>
+        Upload de arquivo
+      </Typography>
+
+      <input
+        accept="*"
+        style={{ display: "none" }}
+        id="file-input"
+        type="file"
+        onChange={(event) => handleFileSelect(event.target.files)}
+        multiple={false}
+      />
+      <label htmlFor="file-input">
+        <Button
+          variant="outlined"
+          color="primary"
+          component="span"
+          startIcon={<UploadFileIcon />}
+          fullWidth
+          style={{ marginBottom: 16 }}
         >
-          <Button
-            component="label"
-            role={undefined}
-            variant="contained"
-            tabIndex={-1}
-            startIcon={<CloudUploadIcon />}
-          >
-            Escolher arquivo
-            <VisuallyHiddenInput
-              type="file"
-              onChange={(event) => setFileState(event.target.files)}
-              multiple={false}
-            />
-          </Button>
-          <Button
-            variant="contained"
-            tabIndex={0}
-            startIcon={<PublishRoundedIcon />}
-            disabled={activeLoader} // O botão é habilitado após a escolha do arquivo
-            onClick={handleFileUpload} // Chama a função para processar o arquivo
-          >
-            Carregar arquivo
-            <VisuallyHiddenInput type="file" />
-          </Button>
-        </Stack>
-      </Box>
-      <Box sx={{ width: "50%", margin: "5px" }}>
-        <Collapse in={open}>
-          <Alert
-            severity="error"
-            action={
-              <IconButton
-                aria-label="close"
-                color="inherit"
-                size="small"
-                onClick={() => {
-                  setOpen(false);
-                }}
-              >
-                <CloseIcon fontSize="inherit" />
-              </IconButton>
-            }
-            sx={{ mb: 2 }}
-          >
-            {message}
-          </Alert>
-        </Collapse>
-        {loaded && <motion.div layout
-          className="loaded"
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            duration: 0.8,
-            delay: 0.5,
-            ease: [0, 0.71, 0.2, 1.01],
-          }}
-          style={{textAlign: 'center'}}
-        ><Typography align="center" variant="button" style={{}}>Arquivo Carregado com sucesso!</Typography></motion.div>}
-        
-      </Box>
+          Escolher Arquivo
+        </Button>
+      </label>
+
+      {selectedFile && (
+        <Typography variant="body1" gutterBottom>
+          Arquivo Selecionado: {selectedFile.name}
+        </Typography>
+      )}
+
+      <Button
+        variant="contained"
+        color="primary"
+        startIcon={
+          uploading ? <CircularProgress size={24} /> : <CloudUploadIcon />
+        }
+        onClick={handleFileUpload}
+        disabled={!selectedFile} // Caso não tenha sido carregado um arquivo, o botão de carregar deve ser mantido desabilitado
+        fullWidth
+      >
+        {uploading ? "Carregando..." : "Carregar Arquivo"}
+      </Button>
+
+      {/* Posicionando o CustomAlert na parte inferior direita */}
+      {alert && (
+        <Box position="fixed" bottom={16} right={16} zIndex={9999}>
+          <CustomAlert type={alert.type} message={alert.message} setFatherState={() => setAlert(null)} />
+        </Box>
+      )}
     </Box>
   );
 }
