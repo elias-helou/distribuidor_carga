bd3a6a96aad322af59633c74f296559c829b1e4a
José Bieco <jo_bieco@hotmail.com>
2024-09-28 00:52:14 -0300

    feat(src/algorithms/buscaTbu.tsx): implementação das funções podeAtribuir e avaliarSolucao

--- src/algorithms/buscaTabu.ts
+++ src/algorithms/buscaTabu.ts
@@ -1,6 +1,3 @@
-import { Atribuicao, Disciplina, Docente } from "@/context/Global/utils";
-import { Solucao } from "./utils";
-
 /**
  * Busca Tabu
  * Essa função será a principal, agrupando as chamadas para as demais funções.
@@ -38,6 +35,9 @@ import { Solucao } from "./utils";
  *      - O docente não pode ser atribuído a disciplina se apresentar choque de horário com outra que ele tenha sido alocado.
  *          ** A interface disciplina apresenta a propriedade conflitos, a qual previamente foi gerada. Essa propriedade apresenta
  *              todas as disciplinas que tenham choque de horário com a "atual".
+ *      - O docente só pode ser atribuído se a disciplina não houver outro docente atribuído.
+ *          ** Nesses casos serão aceitas somente trocas.
+ *            *** Verificar se faz sentido essa verificação nesta função ***
  * 
  * A avaliação será gerada dentro da função `gerarVizinhos` por uma outra função chamada `avaliarSolucao`. 
  * Essa função atribuirá pontos a solução enviada como parâmetro segundo as seguintes regras:
@@ -61,12 +61,8 @@ import {
   Docente,
   Atribuicao,
   Formulario,
-  isHorario,
-  horariosSobrepoem,
 } from "@/context/Global/utils";
 import {
-  atualizarListaTabu,
-  estaNaListaTabu,
   Solucao,
 } from "./utils";
 import { Dispatch, SetStateAction } from "react";
@@ -78,20 +74,57 @@ function podeAtribuir(
   travas: Trava[],
   atribuicoes: Atribuicao[],
   disciplinas: Disciplina[],
-  formularios: Formulario[]
 ): boolean {
+  if (!disciplina.ativo || !docente.ativo) {
+    return false;
+  }
 
+  if (travas.some(trava => trava.id_disciplina === disciplina.id && trava.nome_docente === docente.nome)) {
+    return false;
+  }
+
+  // Pensar se eu devo verificar aqui se está na lista tabu
+
+  // Verifica se o docente apresenta um formulário para a disciplina
+  if (!docente.formularios.has(disciplina.id)) {
+    return false;
+  }
+
+  // Verifica se o docente não foi alocado em outra disciplina que apresente choque de horário
+  const docenteAtribuicoes: string[] = atribuicoes.filter(atribuicao => atribuicao.docentes.includes(docente.nome))
+                                                    .map(atribuicao => atribuicao.id_disciplina)
+  if(disciplinas.some(disc => docenteAtribuicoes.includes(disc.id) && disc.conflitos.has(disciplina.id))) {
+    return false;
+  }
+
+  // A disciplina não pode apresentar mais de um docente
+  const discAtribuicao: Atribuicao = atribuicoes.find(atribuicao => atribuicao.id_disciplina === disciplina.id)
+  if(discAtribuicao.docentes.length === 1) {
+    return false
+  }
+
+  return true;
 }
 
 // Função para avaliar uma solução
 function avaliarSolucao(
   atribuicoes: Atribuicao[],
-  formularios: Formulario[],
   docentes: Docente[],
-  disciplinas: Disciplina[],
   maiorPrioridade: number
 ): number {
+  let avaliacao = 0;
 
+  // Tendo em vista que as atribuições são feotas apenas para docentes que apresentem formulários preenchidos.
+  for(const atribuicao of atribuicoes) {
+    // Mesmo tendo uma trava para apeas um docente alocado por disciplina, o código já foi elaborado para futuramente
+    // aceitar o comportamento de mais de um docente alocado.
+    for(const docenteAtribuido of atribuicao.docentes) {
+      const doc = docentes.find(obj => obj.nome === docenteAtribuido);
+      avaliacao += maiorPrioridade - doc.formularios.get(atribuicao.id_disciplina)
+    }
+  }
+
+  return avaliacao;
 }
 
 
@@ -117,7 +150,7 @@ function gerarVizinhos(
   formularios: Formulario[],
   maiorPrioridade: number
 ): Solucao[] {
- 
+
 }
 
 
@@ -142,7 +175,7 @@ export async function buscaTabu(
   setIteracoes: Dispatch<SetStateAction<number>>
 ): Promise<Solucao> {
 
-  
+
 }
 
 
