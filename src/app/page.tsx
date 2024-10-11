9849b06736f66a1472450fabc951ce12fbd619f1
José Bieco <jo_bieco@hotmail.com>
2024-10-11 09:11:49 -0300

    feat(src/app): Componente para apresentar os autores na página home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,41 @@
-"use client"
+"use client";
+
+import AuthProfile, { IAuthProfileProps } from "@/components/AuthProfile";
+import {
+  Container,
+  Grid2,
+  Typography,
+} from "@mui/material";
 
 export default function Home() {
+  const autores: IAuthProfileProps[] = [
+    { name: "José Eduardo Saroba Bieco", email: "jose.bieco@usp.br", lattes: "http://lattes.cnpq.br/1790961525430099"},
+    { name: "Elias Salomão Helou Neto", email: "elias@icmc.usp.br", lattes: "http://lattes.cnpq.br/5434724108176150" },
+  ];
 
   return (
-    <div>
-      <h1>Autores</h1>
-      <ul>  
-        <li> José Bieco <a href="mailto:jose.bieco@usp.br">jose.bieco@usp.br</a></li>
-        <li> Elias Salomão Helou Neto  <a href="mailto:elias@icmc.usp.br">elias@icmc.usp.br</a></li>
-      </ul>
-    </div>
+    <Container maxWidth="sm">
+      {/* <h1>Autores</h1>
+      <ul>
+        <li>
+          {" "}
+          José Eduardo Saroba Bieco{" "}
+          <a href="mailto:jose.bieco@usp.br">jose.bieco@usp.br</a>
+        </li>
+        <li>
+          {" "}
+          Elias Salomão Helou Neto{" "}
+          <a href="mailto:elias@icmc.usp.br">elias@icmc.usp.br</a>
+        </li>
+      </ul> */}
+      <Grid2 container spacing={2} alignItems="center" justifyContent="center">
+        <Typography variant="h4" component="h1" align="center" fontWeight="bold">Autores</Typography>
+        <Grid2 container spacing={4} alignItems="center" justifyContent="center">
+          {autores.map((autor) => (
+            <AuthProfile key={autor.email} name={autor.name} email={autor.email} lattes={autor.lattes}/>
+          ))}
+        </Grid2>
+      </Grid2>
+    </Container>
   );
 }
---
+++ src/components/AuthProfile.tsx
@@ -0,0 +1,102 @@
+import {
+  Grid2,
+  IconButton,
+  Link,
+  Paper,
+  Stack,
+  Tooltip,
+  Typography,
+  Divider,
+  Box,
+  styled
+} from "@mui/material";
+
+
+import EmailIcon from "@mui/icons-material/Email";
+import DescriptionIcon from '@mui/icons-material/Description';
+
+import { motion } from "framer-motion"
+
+export interface IAuthProfileProps {
+  name: string;
+  email: string;
+  lattes: string
+}
+
+const StyledPaper = styled(Paper)(({theme}) =>({
+    transition: theme.transitions.create(['transform'], {
+    duration: theme.transitions.duration.standard,
+  }),
+  "&:hover": { transform: "scale3d(1.1, 1.1, 1)", transition: theme.transitions.duration.standard }
+}))
+
+export default function AuthProfile(props: IAuthProfileProps) {
+  return (
+    <motion.div
+        // initial={{ opacity: 0, scale: 0.5 }}
+        // animate={{ opacity: 1, scale: 1 }}
+        // transition={{
+        //     duration: 0.3,
+        //     ease: [0, 0.71, 0.2, 1.01],
+        //     scale: {
+        //     type: "spring",
+        //     damping: 5,
+        //     stiffness: 100,
+        //     restDelta: 0.001
+        //     }
+        // }}
+        
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+            duration: 0.8,
+            delay: 0.2,
+            ease: [0, 0.71, 0.2, 1.01]
+        }}
+    >
+    <StyledPaper elevation={2}>
+      <Grid2
+        size="grow"
+        alignContent="center"
+        textAlign="center"
+        justifyContent="center"
+        padding="5px"
+        minWidth="15rem"
+      >
+        {/* Logo / Imagem */}
+        <Box component="img" 
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Webysther_20170627_-_Logo_ICMC-USP.svg/1920px-Webysther_20170627_-_Logo_ICMC-USP.svg.png"
+        sx={{height: '100px', width: '200px'}}
+        />
+        <Divider />
+        {/* Informações básicas */}
+        <Typography variant="h6" color="textSecondary" textAlign="center">
+          {props.name}
+        </Typography>
+        <Link
+          href={`mailto:${props.email}`}
+          underline="none"
+          textAlign="center"
+          alignContent="center"
+        >
+          {props.email}
+        </Link>
+        <Divider />
+        {/* Botões na parte inferior do profile */}
+        <Stack direction="row" spacing={2} sx={{justifyContent: "center", alignItems: "center"}}>
+            <Tooltip title="Email">
+                <IconButton aria-label="Email" href={`mailto:${props.email}`}>
+                    <EmailIcon fontSize="medium"/>
+                </IconButton>
+            </Tooltip>
+            <Tooltip title="Currículo Lattes">
+                <IconButton aria-label="Lattes" href={props.lattes} target="_blank">
+                    <DescriptionIcon fontSize="medium"/>
+                </IconButton>
+            </Tooltip>
+        </Stack>
+      </Grid2>
+    </StyledPaper>
+    </motion.div>
+  );
+}
