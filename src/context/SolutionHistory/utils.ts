b056056bed6c419a08a70524f0749c6027a2da92
José Bieco <jo_bieco@hotmail.com>
2024-10-11 17:44:40 -0300

    feat(src/context/SolutionHistory): criação de funções que atualizem os states referentes ao histórico.

---
+++ src/context/SolutionHistory/utils.ts
@@ -0,0 +1,40 @@
+import { getFormattedDate } from "@/app/timetable";
+import { HistoricoSolucao, Solucao } from "../Global/utils";
+
+/**
+ * Função que adiciona uma nova solução ao histórico
+ * @param novaSolucao 
+ * @param setHistoricoSolucoes 
+ * @param historicoSolucoes 
+ * @returns {string} Identificador da solução no histórico
+ */
+export function addNewSolutionToHistory(novaSolucao: Solucao, 
+    setHistoricoSolucoes: (historicoSolucoes: Map<string, HistoricoSolucao>) => void,
+    historicoSolucoes: Map<string, HistoricoSolucao>
+): string {
+    // Por conta do React, a inserção de um novo item em um Map deve ser feita da maneira a seguir.
+    const newHistoricoSolucoesMap: Map<string, HistoricoSolucao> = new Map<string, HistoricoSolucao>(historicoSolucoes)
+    
+    const date = new Date()
+    const id = getFormattedDate(date)
+    newHistoricoSolucoesMap.set(id, {id: id, solucao: novaSolucao, datetime: date.toLocaleString()})
+
+    setHistoricoSolucoes(newHistoricoSolucoesMap)
+
+    return id;
+}
+
+/**
+ * Função que irá atualizar o state solucaoAtual, que acabou de ser inserida no histórico, adicionando um identificador.
+ * @param setSolucaoAtual Função que atribuí o state da solução atual
+ * @param {string} id Identificador da solução no histórico 
+ */
+export function updateSolutionId(
+  setSolucaoAtual: React.Dispatch<React.SetStateAction<Solucao>>,
+  id: string
+) {
+  setSolucaoAtual((solucaoAtual) => ({
+    ...solucaoAtual,          // Copia as propriedades existentes de Solucao
+    idHistorico: id           // Atualiza apenas o idHistorico
+  }));
+}
