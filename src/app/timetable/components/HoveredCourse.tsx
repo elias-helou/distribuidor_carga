f19bb5805598640dd55d1c7365216b7d07d5cf95
José Bieco <jo_bieco@hotmail.com>
2024-10-19 19:21:48 -0300

    refactor(src/app/timetabling/components): criação de um componente responsável por exibir as informações das disciplinas em um bloco flutuante na tela.

---
+++ src/app/timetable/components/HoveredCourse.tsx
@@ -0,0 +1,89 @@
+import { Disciplina } from "@/context/Global/utils";
+import { Paper, Stack, Typography } from "@mui/material";
+
+interface HoveredCourseProps {
+  disciplina: Disciplina;
+}
+
+/**
+ * Componente desenvolvido para exibir as turmas que forem "selecionadas" no cabeçalho da tabela da tela Timetabling,
+ * com intuíto de exibir toadas as informações sem preocupação com o tamanho do componente.
+ */
+export default function HoveredCourse({ disciplina }: HoveredCourseProps) {
+  /**
+   * Cria o bloco referente aos horários de um adisicplina
+   * @param disciplina Disciplina contendo os horários
+   * @returns Componente React com os horários.
+   */
+  const createHorariosblock = (disciplina: Disciplina): React.ReactNode => {
+    const horarios = disciplina.horarios ?? []; // Caso 'horarios' seja null ou undefined, usa um array vazio.
+
+    return (
+      <Typography
+        align="left"
+        variant="body1"
+        style={{ fontSize: "18px", whiteSpace: "pre-wrap" }}
+      >
+        Horário:
+        {horarios.length > 0 ? (
+          horarios.map((horario, index) =>
+            horario ? (
+              <span key={`${disciplina.nome}-${index}`}>
+                <br />
+                &emsp;{horario.dia} {horario.inicio}/{horario.fim}
+              </span>
+            ) : null
+          )
+        ) : (
+          <span>
+            <br />
+            &emsp;A definir
+          </span>
+        )}
+      </Typography>
+    );
+  };
+
+  return (
+    <Paper
+      elevation={8}
+      sx={{ position: "absolute", zIndex: 99, bottom: "10vh", right: "2vw" }}
+    >
+      <Stack
+        direction="column"
+        sx={{
+          gap: 2,
+          flexShrink: 0,
+          alignSelf: { xs: "flex-end", sm: "center" },
+          width: "100%",
+          padding: "1em",
+          backgroundColor: "rgba(25, 118, 210, 0.12)",
+        }}
+      >
+        <Typography
+          align="left"
+          variant="body1"
+          style={{ fontWeight: "bold", paddingTop: "2px", fontSize: "22px" }} // Aplica estilos dinâmicos
+          dangerouslySetInnerHTML={{
+            __html: disciplina.cursos
+              .replace(/^[^;]*;/, "")
+              .replace(/<br\s*\/?>/gi, "")
+              .replace(/&emsp;/gi, " "),
+          }}
+        />
+        <Typography
+          align="left"
+          variant="body1"
+          style={{
+            fontWeight: "bold",
+            fontSize: "20px",
+            paddingTop: "2px",
+          }} // Estilos dinâmicos
+        >
+          {disciplina.codigo + " " + disciplina.nome}
+        </Typography>
+        {createHorariosblock(disciplina)}
+      </Stack>
+    </Paper>
+  );
+}
