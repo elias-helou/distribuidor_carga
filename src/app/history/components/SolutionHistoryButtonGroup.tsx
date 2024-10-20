a9b46bdda13b33291a1ff7f759949acc86c58663
José Bieco <jo_bieco@hotmail.com>
2024-10-19 23:10:23 -0300

    feat(src/app/history/components/SolutionHistoryButtonGroup): adição do comportamento de download na tela de histórico.

--- src/app/history/components/SolutionHistoryButtonGroup.tsx
+++ src/app/history/components/SolutionHistoryButtonGroup.tsx
@@ -1,21 +1,46 @@
 import { ButtonGroup, IconButton, Tooltip } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import RestoreIcon from "@mui/icons-material/Restore";
+import DownloadIcon from "@mui/icons-material/Download";
 import { useSolutionHistory } from "@/context/SolutionHistory/hooks";
 
 interface SolutionHistoryButtonGroupProps {
   id: string;
   remove: (id: string) => void;
-  restore: (id: string) => void
+  restore: (id: string) => void;
+  download: (id: string) => void;
 }
 
 const SolutionHistoryButtonGroup: React.FC<SolutionHistoryButtonGroupProps> = ({
-  id, remove, restore
+  id, remove, restore, download
 }) => {
   const {solucaoAtual} = useSolutionHistory()
 
   return (
     <ButtonGroup key={`buttonGroup_${id}`}>
+            <Tooltip
+        title="Restaurar"
+        slotProps={{
+          popper: {
+            modifiers: [
+              {
+                name: "offset",
+                options: {
+                  offset: [0, -5],
+                },
+              },
+            ],
+          },
+        }}
+        arrow
+      >
+        <span> {/** Pela documentação do Material UI deve ser feito dessa forma quando existe a possibilidade do botão ser desativado*/}
+          <IconButton key={`restoreButton_${id}`} color="primary" onClick={() => restore(id)} disabled={solucaoAtual.idHistorico === id}>
+            <RestoreIcon key={`restoreIcon_${id}`} />
+          </IconButton>
+        </span>
+      </Tooltip>
+      
       <Tooltip
         title="Remover"
         slotProps={{
@@ -40,27 +65,25 @@ const SolutionHistoryButtonGroup: React.FC<SolutionHistoryButtonGroupProps> = ({
       </Tooltip>
       
       <Tooltip
-        title="Restaurar"
-        slotProps={{
-          popper: {
-            modifiers: [
-              {
-                name: "offset",
-                options: {
-                  offset: [0, -5],
+          title="Download"
+          slotProps={{
+            popper: {
+              modifiers: [
+                {
+                  name: "offset",
+                  options: {
+                    offset: [0, -5],
+                  },
                 },
-              },
-            ],
-          },
-        }}
-        arrow
-      >
-        <span> {/** Pela documentação do Material UI deve ser feito dessa forma quando existe a possibilidade do botão ser desativado*/}
-          <IconButton key={`restoreButton_${id}`} color="primary" onClick={() => restore(id)} disabled={solucaoAtual.idHistorico === id}>
-            <RestoreIcon key={`restoreIcon_${id}`} />
+              ],
+            },
+          }}
+          arrow
+        >
+          <IconButton key={`downloadButton_${id}`} color="primary" onClick={() => download(id)}>
+            <DownloadIcon key={`downloadIcon_${id}`}/>
           </IconButton>
-        </span>
-      </Tooltip>
+        </Tooltip>
     </ButtonGroup>
   );
 };
