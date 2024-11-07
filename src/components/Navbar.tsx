b6c403fe0964c6dbf008056f0a4d1dc993fd5143
José Bieco <jo_bieco@hotmail.com>
2024-11-07 17:33:16 -0300

    feat(src\components\Navbar.tsx): adição dá pgágina restrições

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import Link from "next/link";
-import { usePathname } from 'next/navigation'
+import { usePathname } from "next/navigation";
 
 interface IPages {
   name: string;
@@ -19,28 +19,38 @@ const navItems: IPages[] = [
   { name: "Carregar dados", link: "/inputfile" },
   { name: "Atribuições", link: "/atribuicoes" },
   { name: "Seleção", link: "/select" },
-  { name: "Histórico", link: "/history"},
-  {name: "Parâmetros", link: "parameters"}
+  { name: "Histórico", link: "/history" },
+  { name: "Parâmetros", link: "parameters" },
+  { name: "Restrições", link: "/restricoes" },
 ];
 
 export default function Navbar() {
-  const pathname  = usePathname()
+  const pathname = usePathname();
 
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar component="nav" position="relative">
+      <AppBar component="nav" position="static">
         <Toolbar>
-          <Box sx={{ display: { xs: "none", sm: "block"} }}>
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
             {navItems.map((item) => (
-                <Button key={item.name} sx={{ color: "#fff", padding: 0, margin: '5px'}} disabled={pathname == item.link} variant={`${pathname == item.link ? 'contained' : 'text'}`}>
-                  <Link
-                    href={item.link}
-                    style={{ textDecoration: "none", color: "white", padding: '5px' }}
-                  >
-                    {item.name}
-                  </Link>
-                </Button>
+              <Button
+                key={item.name}
+                sx={{ color: "#fff", padding: 0, margin: "5px" }}
+                disabled={pathname == item.link}
+                variant={`${pathname == item.link ? "contained" : "text"}`}
+              >
+                <Link
+                  href={item.link}
+                  style={{
+                    textDecoration: "none",
+                    color: "white",
+                    padding: "5px",
+                  }}
+                >
+                  {item.name}
+                </Link>
+              </Button>
             ))}
           </Box>
         </Toolbar>
