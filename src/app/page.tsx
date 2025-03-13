a93d15443330e8aef4a611d7bb42f476de8236de
José Bieco <jo_bieco@hotmail.com>
2025-03-13 15:31:58 -0300

    formatação

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,20 @@
 "use client";
 
 import AuthProfile, { IAuthProfileProps } from "@/components/AuthProfile";
-import {
-  Container,
-  Grid2,
-  Typography,
-} from "@mui/material";
+import { Container, Grid2, Typography } from "@mui/material";
 
 export default function Home() {
   const autores: IAuthProfileProps[] = [
-    { name: "José Eduardo Saroba Bieco", email: "jose.bieco@usp.br", lattes: "http://lattes.cnpq.br/1790961525430099"},
-    { name: "Elias Salomão Helou Neto", email: "elias@icmc.usp.br", lattes: "http://lattes.cnpq.br/5434724108176150" },
+    {
+      name: "José Eduardo Saroba Bieco",
+      email: "jose.bieco@usp.br",
+      lattes: "http://lattes.cnpq.br/1790961525430099",
+    },
+    {
+      name: "Elias Salomão Helou Neto",
+      email: "elias@icmc.usp.br",
+      lattes: "http://lattes.cnpq.br/5434724108176150",
+    },
   ];
 
   return (
@@ -29,10 +33,27 @@ export default function Home() {
         </li>
       </ul> */}
       <Grid2 container spacing={2} alignItems="center" justifyContent="center">
-        <Typography variant="h4" component="h1" align="center" fontWeight="bold">Autores</Typography>
-        <Grid2 container spacing={4} alignItems="center" justifyContent="center">
+        <Typography
+          variant="h4"
+          component="h1"
+          align="center"
+          fontWeight="bold"
+        >
+          Autores
+        </Typography>
+        <Grid2
+          container
+          spacing={4}
+          alignItems="center"
+          justifyContent="center"
+        >
           {autores.map((autor) => (
-            <AuthProfile key={autor.email} name={autor.name} email={autor.email} lattes={autor.lattes}/>
+            <AuthProfile
+              key={autor.email}
+              name={autor.name}
+              email={autor.email}
+              lattes={autor.lattes}
+            />
           ))}
         </Grid2>
       </Grid2>
