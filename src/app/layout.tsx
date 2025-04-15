eb26c204517aaad973673e12463d41d38960b354
José Bieco <jo_bieco@hotmail.com>
2025-04-15 08:45:46 -0300

    fix(src/components/Navbar.tsx): correção do comportamento de responsividade para tela pequenas.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body>
+      <body style={{ margin: 0 }}>
         <Navbar />
         <GlobalWrapper>
           <ProcessWrapper>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,13 @@
 import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
+import MenuIcon from "@mui/icons-material/Menu";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { motion } from "framer-motion";
-import { useTheme } from "@mui/material";
+import { Container, IconButton, Menu, MenuItem, useTheme } from "@mui/material";
 
 interface IPages {
   name: string;
@@ -33,20 +33,128 @@ export default function Navbar() {
   const pathname = usePathname();
   const theme = useTheme();
 
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar
-        component="nav"
-        position="static"
-        sx={{
-          backgroundColor: theme.palette.primary.main,
-          boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
-          //borderRadius: "0px 0px 15px 15px",
-        }}
-      >
-        <Toolbar>
-          <Box sx={{ display: { xs: "none", sm: "flex" }, gap: 2 }}>
+    <AppBar
+      component="nav"
+      position="static"
+      sx={{
+        backgroundColor: theme.palette.primary.main,
+        boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          {/* Menu hamburger para telas pequenas */}
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="menu"
+              onClick={handleOpenNavMenu}
+              sx={{ color: "#fff" }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              anchorEl={anchorElNav}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              sx={{
+                display: { xs: "block", md: "none" },
+                "& .MuiPaper-root": {
+                  backgroundColor: "#1976D2", // mesmo fundo da AppBar
+                },
+              }}
+            >
+              {navItems.map((item) => (
+                <MenuItem
+                  key={item.name}
+                  onClick={handleCloseNavMenu}
+                  sx={{
+                    paddingX: 2,
+                  }}
+                >
+                  <Link href={item.link} passHref legacyBehavior>
+                    <Button
+                      component="a"
+                      fullWidth
+                      disableRipple
+                      disabled={pathname === item.link}
+                      sx={{
+                        justifyContent:
+                          pathname === item.link ? "center" : "flex-start",
+                        color: pathname === item.link ? "#FFFFFF" : "#CFE3FC",
+                        textTransform: "none",
+                        fontWeight: "medium",
+                        textShadow:
+                          pathname === item.link
+                            ? "0px 0px 8px rgba(255, 255, 255, 0.8), 0px 0px 12px rgba(255, 255, 255, 0.6)"
+                            : "none",
+                        "&:hover": {
+                          color: "#D1E9FF",
+                          backgroundColor: "transparent",
+                        },
+                        "&.Mui-disabled": {
+                          color: "#FFFFFF",
+                          textShadow:
+                            "0px 0px 8px rgba(255, 255, 255, 0.8), 0px 0px 12px rgba(255, 255, 255, 0.6)",
+                        },
+                      }}
+                    >
+                      {pathname === item.link && (
+                        <motion.div
+                          layoutId="activeUnderline"
+                          style={{
+                            position: "absolute",
+                            bottom: 0,
+                            left: 0,
+                            right: 0,
+                            height: 4,
+                            backgroundColor: "#FFFFFF",
+                            borderRadius: "2px",
+                          }}
+                          transition={{
+                            type: "spring",
+                            stiffness: 500,
+                            damping: 30,
+                          }}
+                        />
+                      )}
+                      <motion.div
+                        whileHover={{ y: -2 }}
+                        whileTap={{ scale: 0.95 }}
+                        transition={{ type: "spring", stiffness: 300 }}
+                      >
+                        {item.name}
+                      </motion.div>
+                    </Button>
+                  </Link>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          {/* Itens da navbar para telas grandes */}
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {navItems.map((item) => (
               <Link href={item.link} key={item.name} passHref legacyBehavior>
                 <Button
@@ -56,19 +164,19 @@ export default function Navbar() {
                     position: "relative",
                     padding: "10px 20px",
                     fontWeight: "medium",
-                    color: pathname === item.link ? "#90CAF9" : "#E3F2FD",
+                    color: pathname === item.link ? "#FFFFFF" : "#CFE3FC",
                     textShadow:
                       pathname === item.link
-                        ? "0px 0px 8px rgba(144, 202, 249, 0.9), 0px 0px 12px rgba(144, 202, 249, 0.7)"
-                        : "none", // Neon azul em torno do texto branco ativo
+                        ? "0px 0px 8px rgba(255, 255, 255, 0.8), 0px 0px 12px rgba(255, 255, 255, 0.6)"
+                        : "none",
                     "&:hover": {
-                      color: "#BBDEFB",
+                      color: "#D1E9FF",
                       backgroundColor: "transparent",
                     },
                     "&.Mui-disabled": {
-                      color: "#FFFFFF", // Cor branca para o link selecionado
+                      color: "#FFFFFF",
                       textShadow:
-                        "0px 0px 8px rgba(144, 202, 249, 0.9), 0px 0px 12px rgba(144, 202, 249, 0.7)", // Neon azul no link ativo
+                        "0px 0px 8px rgba(255, 255, 255, 0.8), 0px 0px 12px rgba(255, 255, 255, 0.6)",
                     },
                   }}
                   disableRipple
@@ -83,7 +191,7 @@ export default function Navbar() {
                         left: 0,
                         right: 0,
                         height: 4,
-                        backgroundColor: "#BBDEFB",
+                        backgroundColor: "#FFFFFF",
                         borderRadius: "2px",
                       }}
                       transition={{
@@ -105,7 +213,7 @@ export default function Navbar() {
             ))}
           </Box>
         </Toolbar>
-      </AppBar>
-    </Box>
+      </Container>
+    </AppBar>
   );
 }
